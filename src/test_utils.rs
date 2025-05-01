12766375f99c76185db4aabdff17e9ed8dfd243b
benalleng <benalleng@gmail.com>
2025-05-01 09:47:29 -0400

    Introduce bitcoin_ffi Psbt struct and test_utils runtime
    We need more control over psbts in the integration tests so
    I am using my branch of bitcoin_ffi until we have that PR merged
    into main.

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,18 +1,33 @@
+use std::io;
 use std::sync::Arc;
 
+use bitcoin_ffi::Psbt;
+use lazy_static::lazy_static;
 use payjoin_test_utils::{
     EXAMPLE_URL, INVALID_PSBT, ORIGINAL_PSBT, PARSED_ORIGINAL_PSBT, PARSED_PAYJOIN_PROPOSAL,
     PARSED_PAYJOIN_PROPOSAL_WITH_SENDER_INFO, PAYJOIN_PROPOSAL, PAYJOIN_PROPOSAL_WITH_SENDER_INFO,
     QUERY_PARAMS, RECEIVER_INPUT_CONTRIBUTION,
 };
+use tokio::runtime::Runtime;
 use tokio::sync::Mutex;
 
 use crate::Url;
+
+lazy_static! {
+    static ref RUNTIME: Arc<std::sync::Mutex<Runtime>> =
+        Arc::new(std::sync::Mutex::new(Runtime::new().expect("Failed to create Tokio runtime")));
+}
 
 #[derive(Debug, thiserror::Error)]
 #[error(transparent)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Object))]
 pub struct BoxSendSyncError(#[from] payjoin_test_utils::BoxSendSyncError);
+
+impl From<io::Error> for BoxSendSyncError {
+    fn from(err: io::Error) -> Self {
+        payjoin_test_utils::BoxSendSyncError::from(err).into()
+    }
+}
 
 #[cfg_attr(feature = "uniffi", uniffi::export)]
 pub fn init_tracing() {
@@ -43,55 +58,66 @@ impl From<TestServices> for payjoin_test_utils::TestServices {
 #[cfg_attr(feature = "uniffi", uniffi::export)]
 impl TestServices {
     #[cfg_attr(feature = "uniffi", uniffi::constructor)]
-    pub async fn initialize() -> Result<Self, BoxSendSyncError> {
-        Ok(payjoin_test_utils::TestServices::initialize().await?.into())
+    pub fn initialize() -> Result<Self, BoxSendSyncError> {
+        let runtime = RUNTIME.lock().unwrap();
+        let service = runtime.block_on(async {
+            payjoin_test_utils::TestServices::initialize().await.map_err(|e| {
+                eprintln!("Initialization failed: {e}");
+                BoxSendSyncError::from(e)
+            })
+        })?;
+        Ok(TestServices(Mutex::new(service)))
     }
 
-    pub async fn cert(&self) -> Vec<u8> {
-        self.0.lock().await.cert()
+    pub fn cert(&self) -> Vec<u8> {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime.block_on(async { self.0.lock().await.cert() })
     }
 
-    pub async fn directory_url(&self) -> Url {
-        self.0.lock().await.directory_url().into()
+    pub fn directory_url(&self) -> Url {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime.block_on(async { self.0.lock().await.directory_url().into() })
     }
 
-    pub async fn take_directory_handle(&self) -> JoinHandle {
-        JoinHandle(Arc::new(self.0.lock().await.take_directory_handle()))
+    pub fn take_directory_handle(&self) -> JoinHandle {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime
+            .block_on(async { JoinHandle(Arc::new(self.0.lock().await.take_directory_handle())) })
     }
 
-    pub async fn ohttp_relay_url(&self) -> Url {
-        self.0.lock().await.ohttp_relay_url().into()
+    pub fn ohttp_relay_url(&self) -> Url {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime.block_on(async { self.0.lock().await.ohttp_relay_url().into() })
     }
 
-    pub async fn ohttp_gateway_url(&self) -> Url {
-        self.0.lock().await.ohttp_gateway_url().into()
+    pub fn ohttp_gateway_url(&self) -> Url {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime.block_on(async { self.0.lock().await.ohttp_gateway_url().into() })
     }
 
-    pub async fn take_ohttp_relay_handle(&self) -> JoinHandle {
-        JoinHandle(Arc::new(self.0.lock().await.take_ohttp_relay_handle()))
+    pub fn take_ohttp_relay_handle(&self) -> JoinHandle {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime
+            .block_on(async { JoinHandle(Arc::new(self.0.lock().await.take_ohttp_relay_handle())) })
     }
 
-    pub async fn wait_for_services_ready(&self) -> Result<(), BoxSendSyncError> {
-        self.0
-            .lock()
-            .await
-            .wait_for_services_ready()
-            .await
-            .map_err(|e| payjoin_test_utils::BoxSendSyncError::from(e).into())
+    pub fn wait_for_services_ready(&self) -> Result<(), BoxSendSyncError> {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime.block_on(async {
+            self.0
+                .lock()
+                .await
+                .wait_for_services_ready()
+                .await
+                .map_err(|e| payjoin_test_utils::BoxSendSyncError::from(e).into())
+        })
     }
 
-    pub async fn fetch_ohttp_keys(&self) -> Result<crate::OhttpKeys, crate::io::IoError> {
-        self.0.lock().await.fetch_ohttp_keys().await.map_err(Into::into).map(Into::into)
-    }
-}
-
-#[cfg_attr(feature = "uniffi", derive(uniffi::Object))]
-#[derive(Debug)]
-pub struct Psbt(#[allow(dead_code)] pub(crate) std::sync::Mutex<payjoin::bitcoin::Psbt>);
-
-impl From<payjoin::bitcoin::Psbt> for Psbt {
-    fn from(psbt: payjoin::bitcoin::Psbt) -> Self {
-        Self(std::sync::Mutex::new(psbt))
+    pub fn fetch_ohttp_keys(&self) -> Result<crate::OhttpKeys, crate::io::IoError> {
+        let runtime = RUNTIME.lock().unwrap();
+        runtime.block_on(async {
+            self.0.lock().await.fetch_ohttp_keys().await.map_err(Into::into).map(Into::into)
+        })
     }
 }
 
